b3e8d4cb55244767a3a23a977cc98962c5e5ff43
eric <1048315650@qq.com>
2024-12-11 18:08:20 +0800

    feat(rawjson): 支持 rawjson 字段, 用于自定义任意参数

--- khttp/rawjson.marshal.go
+++ khttp/rawjson.marshal.go
@@ -5,3 +5,7 @@ import "encoding/json"
 func (x *RawJson) MarshalJSON() ([]byte, error) {
 	return json.Marshal(json.RawMessage(x.Json))
 }
+
+func NewRawJSON(message json.RawMessage) *RawJson {
+	return &RawJson{Json: message}
+}
